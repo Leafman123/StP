b2cfd4fe661d55503bfc742b25364708e2a9805b
Leafman123 <Minipp123>
2016-08-14 14:52:07 +0100

    added captcha

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ import datetime
 import os
 from _socket import gethostname
 
+import requests
 from flask import Flask, render_template_string, render_template, request, flash, current_app, url_for, redirect
 from flask.ext.user.views import _send_registered_email, _endpoint_url, _do_login_user
 from flask_mail import Mail, signals
@@ -19,7 +20,7 @@ app = Flask(__name__)
 class ConfigClass(object):
     # Flask settings
     SECRET_KEY = os.getenv('SECRET_KEY', 'THIS IS AN INSECURE SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///basic_app.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATAfBASE_URL', 'sqlite:///basic_app.sqlite')
     CSRF_ENABLED = True
 
     # Flask-Mail settings
@@ -32,6 +33,12 @@ class ConfigClass(object):
     MAIL_USE_TLS = int(os.getenv('MAIL_USE_TLS', True))
     # Flask-User settings
     USER_APP_NAME = "Spoon The Prune"  # Used by email templates
+
+
+def captcha_test(request):
+    payload = {'secret': os.getenv('CAPTCHA_SECRET'), 'response': request.values['g-recaptcha-response'] }
+    g_result = requests.post('https://www.google.com/recaptcha/api/siteverify', payload )
+    return '"success": true,' in g_result.text
 
 
 def create_app():
@@ -67,14 +74,168 @@ def create_app():
     # Create all database tables
     db.create_all()
 
+    def my_register():
+
+        user_manager = current_app.user_manager
+        db_adapter = user_manager.db_adapter
+
+        next = request.args.get('next', _endpoint_url(user_manager.after_login_endpoint))
+        reg_next = request.args.get('reg_next', _endpoint_url(user_manager.after_register_endpoint))
+
+        # Initialize form
+        login_form = user_manager.login_form()  # for login_or_register.html
+        register_form = user_manager.register_form(request.form)  # for register.html
+
+        # invite token used to determine validity of registeree
+        invite_token = request.values.get("token")
+
+        # require invite without a token should disallow the user from registering
+        if user_manager.require_invitation and not invite_token:
+            flash("Registration is invite only", "error")
+            return redirect(url_for('user.login'))
+
+        user_invite = None
+        if invite_token and db_adapter.UserInvitationClass:
+            user_invite = db_adapter.find_first_object(db_adapter.UserInvitationClass, token=invite_token)
+            if user_invite:
+                register_form.invite_token.data = invite_token
+            else:
+                flash("Invalid invitation token", "error")
+                return redirect(url_for('user.login'))
+
+        if request.method != 'POST':
+            login_form.next.data = register_form.next.data = next
+            login_form.reg_next.data = register_form.reg_next.data = reg_next
+            if user_invite:
+                register_form.email.data = user_invite.email
+
+
+
+        # Process valid POST
+        if request.method == 'POST' and captcha_test(request) and register_form.validate():
+            # Create a User object using Form fields that have a corresponding User field
+            User = db_adapter.UserClass
+            user_class_fields = User.__dict__
+            user_fields = {}
+
+            # Create a UserEmail object using Form fields that have a corresponding UserEmail field
+            if db_adapter.UserEmailClass:
+                UserEmail = db_adapter.UserEmailClass
+                user_email_class_fields = UserEmail.__dict__
+                user_email_fields = {}
+
+            # Create a UserAuth object using Form fields that have a corresponding UserAuth field
+            if db_adapter.UserAuthClass:
+                UserAuth = db_adapter.UserAuthClass
+                user_auth_class_fields = UserAuth.__dict__
+                user_auth_fields = {}
+
+            # Enable user account
+            if db_adapter.UserProfileClass:
+                if hasattr(db_adapter.UserProfileClass, 'active'):
+                    user_auth_fields['active'] = True
+                elif hasattr(db_adapter.UserProfileClass, 'is_enabled'):
+                    user_auth_fields['is_enabled'] = True
+                else:
+                    user_auth_fields['is_active'] = True
+            else:
+                if hasattr(db_adapter.UserClass, 'active'):
+                    user_fields['active'] = True
+                elif hasattr(db_adapter.UserClass, 'is_enabled'):
+                    user_fields['is_enabled'] = True
+                else:
+                    user_fields['is_active'] = True
+
+            # For all form fields
+            for field_name, field_value in register_form.data.items():
+                # Hash password field
+                if field_name == 'password':
+                    hashed_password = user_manager.hash_password(field_value)
+                    if db_adapter.UserAuthClass:
+                        user_auth_fields['password'] = hashed_password
+                    else:
+                        user_fields['password'] = hashed_password
+                # Store corresponding Form fields into the User object and/or UserProfile object
+                else:
+                    if field_name in user_class_fields:
+                        user_fields[field_name] = field_value
+                    if db_adapter.UserEmailClass:
+                        if field_name in user_email_class_fields:
+                            user_email_fields[field_name] = field_value
+                    if db_adapter.UserAuthClass:
+                        if field_name in user_auth_class_fields:
+                            user_auth_fields[field_name] = field_value
+
+            # Add User record using named arguments 'user_fields'
+            user = db_adapter.add_object(User, **user_fields)
+            if db_adapter.UserProfileClass:
+                user_profile = user
+
+            # Add UserEmail record using named arguments 'user_email_fields'
+            if db_adapter.UserEmailClass:
+                user_email = db_adapter.add_object(UserEmail,
+                                                   user=user,
+                                                   is_primary=True,
+                                                   **user_email_fields)
+            else:
+                user_email = None
+
+            # Add UserAuth record using named arguments 'user_auth_fields'
+            if db_adapter.UserAuthClass:
+                user_auth = db_adapter.add_object(UserAuth, **user_auth_fields)
+                if db_adapter.UserProfileClass:
+                    user = user_auth
+                else:
+                    user.user_auth = user_auth
+
+            require_email_confirmation = True
+            if user_invite:
+                if user_invite.email == register_form.email.data:
+                    require_email_confirmation = False
+                    db_adapter.update_object(user, confirmed_at=datetime.utcnow())
+
+            db_adapter.commit()
+
+            # Send 'registered' email and delete new User object if send fails
+            if user_manager.send_registered_email:
+                try:
+                    # Send 'registered' email
+                    _send_registered_email(user, user_email, require_email_confirmation)
+                except Exception as e:
+                    # delete new User object if send  fails
+                    db_adapter.delete_object(user)
+                    db_adapter.commit()
+                    raise
+
+            # Send user_registered signal
+            signals.user_registered.send(current_app._get_current_object(),
+                                         user=user,
+                                         user_invite=user_invite)
+
+            # Redirect if USER_ENABLE_CONFIRM_EMAIL is set
+            if user_manager.enable_confirm_email and require_email_confirmation:
+                next = request.args.get('next', _endpoint_url(user_manager.after_register_endpoint))
+                return redirect(next)
+
+            # Auto-login after register or redirect to login page
+            next = request.args.get('next', _endpoint_url(user_manager.after_confirm_endpoint))
+            if user_manager.auto_login_after_register:
+                return _do_login_user(user, reg_next)  # auto-login
+            else:
+                return redirect(url_for('user.login') + '?next=' + reg_next)  # redirect to login page
+
+        # Process GET or invalid POST
+        return render_template(user_manager.register_template,
+                               form=register_form,
+                               login_form=login_form,
+                               register_form=register_form)
+
     # Setup Flask-User
     db_adapter = SQLAlchemyAdapter(db, User)  # Register the User model
-    user_manager = UserManager(db_adapter, app)  # Initialize Flask-User
+    user_manager = UserManager(db_adapter, app,
+                               register_view_function= my_register)  # Initialize Flask-User
 
     # View functions
-    # user_manager = UserManager(db_adapter,
-    #                            register_view_function='/views.py',
-    #                            )
     # user_manager.init_app(app)
 
     # The Home page is accessible to anyone
@@ -82,159 +243,6 @@ def create_app():
     def home_page():
         return render_template('home.html', title='Home')
 
-    # def register():
-    #
-    #     user_manager = current_app.user_manager
-    #     db_adapter = user_manager.db_adapter
-    #
-    #     next = request.args.get('next', _endpoint_url(user_manager.after_login_endpoint))
-    #     reg_next = request.args.get('reg_next', _endpoint_url(user_manager.after_register_endpoint))
-    #
-    #     # Initialize form
-    #     login_form = user_manager.login_form()  # for login_or_register.html
-    #     register_form = user_manager.register_form(request.form)  # for register.html
-    #
-    #     # invite token used to determine validity of registeree
-    #     invite_token = request.values.get("token")
-    #
-    #     # require invite without a token should disallow the user from registering
-    #     if user_manager.require_invitation and not invite_token:
-    #         flash("Registration is invite only", "error")
-    #         return redirect(url_for('user.login'))
-    #
-    #     user_invite = None
-    #     if invite_token and db_adapter.UserInvitationClass:
-    #         user_invite = db_adapter.find_first_object(db_adapter.UserInvitationClass, token=invite_token)
-    #         if user_invite:
-    #             register_form.invite_token.data = invite_token
-    #         else:
-    #             flash("Invalid invitation token", "error")
-    #             return redirect(url_for('user.login'))
-    #
-    #     if request.method != 'POST':
-    #         login_form.next.data = register_form.next.data = next
-    #         login_form.reg_next.data = register_form.reg_next.data = reg_next
-    #         if user_invite:
-    #             register_form.email.data = user_invite.email
-    #
-    #     # Process valid POST
-    #     if request.method == 'POST' and register_form.validate():
-    #         # Create a User object using Form fields that have a corresponding User field
-    #         User = db_adapter.UserClass
-    #         user_class_fields = User.__dict__
-    #         user_fields = {}
-    #
-    #         # Create a UserEmail object using Form fields that have a corresponding UserEmail field
-    #         if db_adapter.UserEmailClass:
-    #             UserEmail = db_adapter.UserEmailClass
-    #             user_email_class_fields = UserEmail.__dict__
-    #             user_email_fields = {}
-    #
-    #         # Create a UserAuth object using Form fields that have a corresponding UserAuth field
-    #         if db_adapter.UserAuthClass:
-    #             UserAuth = db_adapter.UserAuthClass
-    #             user_auth_class_fields = UserAuth.__dict__
-    #             user_auth_fields = {}
-    #
-    #         # Enable user account
-    #         if db_adapter.UserProfileClass:
-    #             if hasattr(db_adapter.UserProfileClass, 'active'):
-    #                 user_auth_fields['active'] = True
-    #             elif hasattr(db_adapter.UserProfileClass, 'is_enabled'):
-    #                 user_auth_fields['is_enabled'] = True
-    #             else:
-    #                 user_auth_fields['is_active'] = True
-    #         else:
-    #             if hasattr(db_adapter.UserClass, 'active'):
-    #                 user_fields['active'] = True
-    #             elif hasattr(db_adapter.UserClass, 'is_enabled'):
-    #                 user_fields['is_enabled'] = True
-    #             else:
-    #                 user_fields['is_active'] = True
-    #
-    #         # For all form fields
-    #         for field_name, field_value in register_form.data.items():
-    #             # Hash password field
-    #             if field_name == 'password':
-    #                 hashed_password = user_manager.hash_password(field_value)
-    #                 if db_adapter.UserAuthClass:
-    #                     user_auth_fields['password'] = hashed_password
-    #                 else:
-    #                     user_fields['password'] = hashed_password
-    #             # Store corresponding Form fields into the User object and/or UserProfile object
-    #             else:
-    #                 if field_name in user_class_fields:
-    #                     user_fields[field_name] = field_value
-    #                 if db_adapter.UserEmailClass:
-    #                     if field_name in user_email_class_fields:
-    #                         user_email_fields[field_name] = field_value
-    #                 if db_adapter.UserAuthClass:
-    #                     if field_name in user_auth_class_fields:
-    #                         user_auth_fields[field_name] = field_value
-    #
-    #         # Add User record using named arguments 'user_fields'
-    #         user = db_adapter.add_object(User, **user_fields)
-    #         if db_adapter.UserProfileClass:
-    #             user_profile = user
-    #
-    #         # Add UserEmail record using named arguments 'user_email_fields'
-    #         if db_adapter.UserEmailClass:
-    #             user_email = db_adapter.add_object(UserEmail,
-    #                                                user=user,
-    #                                                is_primary=True,
-    #                                                **user_email_fields)
-    #         else:
-    #             user_email = None
-    #
-    #         # Add UserAuth record using named arguments 'user_auth_fields'
-    #         if db_adapter.UserAuthClass:
-    #             user_auth = db_adapter.add_object(UserAuth, **user_auth_fields)
-    #             if db_adapter.UserProfileClass:
-    #                 user = user_auth
-    #             else:
-    #                 user.user_auth = user_auth
-    #
-    #         require_email_confirmation = True
-    #         if user_invite:
-    #             if user_invite.email == register_form.email.data:
-    #                 require_email_confirmation = False
-    #                 db_adapter.update_object(user, confirmed_at=datetime.utcnow())
-    #
-    #         db_adapter.commit()
-    #
-    #         # Send 'registered' email and delete new User object if send fails
-    #         if user_manager.send_registered_email:
-    #             try:
-    #                 # Send 'registered' email
-    #                 _send_registered_email(user, user_email, require_email_confirmation)
-    #             except Exception as e:
-    #                 # delete new User object if send  fails
-    #                 db_adapter.delete_object(user)
-    #                 db_adapter.commit()
-    #                 raise
-    #
-    #         # Send user_registered signal
-    #         signals.user_registered.send(current_app._get_current_object(),
-    #                                      user=user,
-    #                                      user_invite=user_invite)
-    #
-    #         # Redirect if USER_ENABLE_CONFIRM_EMAIL is set
-    #         if user_manager.enable_confirm_email and require_email_confirmation:
-    #             next = request.args.get('next', _endpoint_url(user_manager.after_register_endpoint))
-    #             return redirect(next)
-    #
-    #         # Auto-login after register or redirect to login page
-    #         next = request.args.get('next', _endpoint_url(user_manager.after_confirm_endpoint))
-    #         if user_manager.auto_login_after_register:
-    #             return _do_login_user(user, reg_next)  # auto-login
-    #         else:
-    #             return redirect(url_for('user.login') + '?next=' + reg_next)  # redirect to login page
-    #
-    #     # Process GET or invalid POST
-    #     return render_template(user_manager.register_template,
-    #                            form=register_form,
-    #                            login_form=login_form,
-    #                            register_form=register_form)
 
     @app.route('/')
     def startup():
